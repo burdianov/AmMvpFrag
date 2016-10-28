04f10d9b4061743f0b1680490f2f18e14d980aca
Leo Burdianov <leo.burdianov@gmail.com>
2016-10-28 13:29:49 +0400

    Style the NavigationDrawer

--- app/src/main/java/com/testography/am_mvp/ui/activities/RootActivity.java
+++ app/src/main/java/com/testography/am_mvp/ui/activities/RootActivity.java
@@ -1,6 +1,72 @@
 package com.testography.am_mvp.ui.activities;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-public class RootActivity extends AppCompatActivity {
+import com.testography.am_mvp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RootActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawer;
+    @BindView(R.id.nav_view)
+    NavigationView mNavigationView;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.coordinator_container)
+    CoordinatorLayout mCoordinatorContainer;
+    @BindView(R.id.fragment_container)
+    FrameLayout mFragmentContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_root);
+        ButterKnife.bind(this);
+
+        initToolbar();
+        initDrawer();
+    }
+
+    private void initDrawer() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                mDrawer, mToolbar, R.string.open_drawer, R.string.close_drawer);
+        mDrawer.setDrawerListener(toggle);
+        toggle.syncState();
+        mNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_account:
+                break;
+            case R.id.nav_catalog:
+                break;
+            case R.id.nav_favorites:
+                break;
+            case R.id.nav_orders:
+                break;
+            case R.id.nav_notifications:
+                break;
+        }
+        mDrawer.closeDrawer(GravityCompat.START);
+        return false;
+    }
 }
